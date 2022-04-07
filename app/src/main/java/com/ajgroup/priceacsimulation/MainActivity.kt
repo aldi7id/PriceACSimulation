1f147d9e51219b6313750361cf430dea63f12b5d
aldi7id <aldi7.indo@gmail.com>
2022-04-07 10:02:49 +0700

    Initial commit

---
+++ app/src/main/java/com/ajgroup/priceacsimulation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ajgroup.priceacsimulation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
