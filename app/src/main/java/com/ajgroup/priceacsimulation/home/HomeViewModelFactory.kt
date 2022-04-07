f793138e77304b85e4262211eb3f202cf2e9c05b
aldi7id <aldi7.indo@gmail.com>
2022-04-07 12:12:11 +0700

    add HomeViewModelFactory.kt

--- app/src/main/java/com/ajgroup/priceacsimulation/home/HomeViewModelFactory.kt
+++ app/src/main/java/com/ajgroup/priceacsimulation/home/HomeViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.ajgroup.priceacsimulation.home
 
-class HomeViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ajgroup.priceacsimulation.database.RegisterRepository
+import java.lang.IllegalArgumentException
+
+class HomeViewModelFactory(
+    private  val repository: RegisterRepository,
+    private val application: Application
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(repository, application) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+
+    }
 }
